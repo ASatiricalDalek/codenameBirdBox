c23ba2142e312c704c6b2546d18f7eaec8a675b0
connor <dalek124@gmail.com>
2020-01-09 16:03:43 -0500

    Added shell scripts

--- app/routes.py
+++ app/routes.py
@@ -6,5 +6,11 @@ def hello_world():
     return 'Hello World!'
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+
+@app.route('/test')
+def test():
+    return 'Hello Test!'
+
+
+# if __name__ == '__main__':
+ #   app.run(host='0.0.0.0')
