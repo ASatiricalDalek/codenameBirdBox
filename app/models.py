3fc894f55583a05034b1aaa19b6ef46836cd617a
Connor <dalek124@gmail.com>
2020-01-20 11:33:03 -0500

    1/20/2020 - First migration push

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from app import db
 
 
 # Python object that maps to our users in the database
-class User(db.Model):
+class users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(128))
@@ -10,3 +10,11 @@ class User(db.Model):
     # This built-in function tells Python how to print these objects for debugging purposes
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class attributes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer, db.ForeignKey(users.id))
+    canFeed = db.Column(db.Integer)
+    canView = db.Column(db.Integer)
+    style = db.Column(db.String(32))
