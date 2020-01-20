0ffda9fc732797918ae3c6b34e9c17889cea0c83
Connor <dalek124@gmail.com>
2020-01-20 11:02:33 -0500

    1/20/2020 - Forms implemented

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iii-web-forms
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, BooleanField, StringField, SubmitField
-from wtforms.validators import data_required
+from wtforms.validators import DataRequired
 
 
 class signIn(FlaskForm):
-    username = StringField('Username', validators=['DataRequired()'])
-    password = StringField('Password', validators=['DataRequired()'])
-    remember = BooleanField('Remember Me')
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me', default=False)
     signInBtn = SubmitField('Sign In')
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,17 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import forms
 
-# TODO: Figure out why puttng methods in the decorator breaks Docker container
-@app.route('/', METHODS=['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def startPage():
     login = forms.signIn()
+    # Only run this when the form is submitted, not on page load
+    if login.validate_on_submit():
+        flash('Login requested for user {}, remember={}'.format(login.username.data, login.remember.data))
+        return redirect(url_for('startPage'))
+    else:
+        print(login.errors)
     return render_template('login.html', form=login)
 
 
