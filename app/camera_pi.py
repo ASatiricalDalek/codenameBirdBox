8628e40c7b57548e75a4c4183c4207455e470247
connor <dalek124@gmail.com>
2020-01-28 10:25:42 -0500

    Commented out Pi specific code

--- app/camera_pi.py
+++ app/camera_pi.py
@@ -1,27 +1,27 @@
-# https://github.com/miguelgrinberg/flask-video-streaming
-import io
-import time
-import picamera
-from app import base_camera
-
-
-class Camera(base_camera.BaseCamera):
-    @staticmethod
-    def frames():
-        with picamera.PiCamera() as camera:
-            camera.resolution = (640,480)
-            #camera.framerate = 24 
-            # let camera warm up
-            time.sleep(2)
-
-            stream = io.BytesIO()
-            for _ in camera.capture_continuous(stream, 'jpeg',
-                                                 use_video_port=True):
-                # return current frame
-                stream.seek(0)
-                yield stream.read()
-
-                # reset stream for next frame
-                stream.seek(0)
-                stream.truncate()
-
+# # https://github.com/miguelgrinberg/flask-video-streaming
+# import io
+# import time
+# import picamera
+# from app import base_camera
+#
+#
+# class Camera(base_camera.BaseCamera):
+#     @staticmethod
+#     def frames():
+#         with picamera.PiCamera() as camera:
+#             camera.resolution = (640,480)
+#             #camera.framerate = 24
+#             # let camera warm up
+#             time.sleep(2)
+#
+#             stream = io.BytesIO()
+#             for _ in camera.capture_continuous(stream, 'jpeg',
+#                                                  use_video_port=True):
+#                 # return current frame
+#                 stream.seek(0)
+#                 yield stream.read()
+#
+#                 # reset stream for next frame
+#                 stream.seek(0)
+#                 stream.truncate()
+#
