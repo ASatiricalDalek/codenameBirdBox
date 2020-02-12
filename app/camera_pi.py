93d42e740354a4cd1a0c663273a17af2e26a5e7b
Robert Kazirut <rakazirut@oakland.edu>
2020-02-12 18:19:48 -0500

    + Additional Try except blocks

--- app/camera_pi.py
+++ app/camera_pi.py
@@ -1,31 +1,31 @@
-# # https://github.com/miguelgrinberg/flask-video-streaming
-# import io
-# import time
-# import picamera
-# from app import base_camera, routes
-#
-#
-# class Camera(base_camera.BaseCamera):
-#     # This overrides the frames function in BaseCamera.py
-#     # The  _thread function in BaseCamera uses this function to define the parameters of the stream
-#     def frames():
-#         with picamera.PiCamera() as camera:
-#             # Defines the camera resolution and filter for the camera
-#             camera.resolution = (640, 480)
-#             camera.rotation = 180
-#             camera.image_effect = routes.filter  # References the global filter in routes.py
-#
-#             # let camera warm up
-#             time.sleep(2)
-#
-#             stream = io.BytesIO()
-#             for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
-#                 # return current frame
-#                 stream.seek(0)
-#                 yield stream.read()
-#
-#                 # reset stream for next frame
-#                 stream.seek(0)
-#                 stream.truncate()
-#
-#
+# https://github.com/miguelgrinberg/flask-video-streaming
+import io
+import time
+import picamera
+from app import base_camera, routes
+
+
+class Camera(base_camera.BaseCamera):
+    # This overrides the frames function in BaseCamera.py
+    # The  _thread function in BaseCamera uses this function to define the parameters of the stream
+    def frames():
+        with picamera.PiCamera() as camera:
+            # Defines the camera resolution and filter for the camera
+            camera.resolution = (640, 480)
+            camera.rotation = 180
+            camera.image_effect = routes.filter  # References the global filter in routes.py
+
+            # let camera warm up
+            time.sleep(2)
+
+            stream = io.BytesIO()
+            for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+                # return current frame
+                stream.seek(0)
+                yield stream.read()
+
+                # reset stream for next frame
+                stream.seek(0)
+                stream.truncate()
+
+
