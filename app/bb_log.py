1d2efc442bbf6d54998425007b5e2bf9d5c9b7fc
paul-albrecht <47396932+paul-albrecht@users.noreply.github.com>
2020-03-03 07:55:26 -0500

    Finalized CSS pages
    Finalized CSS pages with some HTML

--- app/bb_log.py
+++ app/bb_log.py
@@ -5,18 +5,18 @@ import shutil
 
 
 def log_path():
-    if os.path.isdir(str(os.getcwd())+'/log'):
-        shutil.rmtree(str(os.getcwd())+'/log')  # removes log directory and files contained within
-        os.mkdir(str(os.getcwd())+'/log')  # Created log directory for the log file associated with the current session
+    if os.path.isdir(str(os.getcwd())+'\\log'):
+        shutil.rmtree(str(os.getcwd())+'\\log')  # removes log directory and files contained within
+        os.mkdir(str(os.getcwd())+'\\log')  # Created log directory for the log file associated with the current session
         log_format()  # define log format
     else:
-        os.mkdir(str(os.getcwd()) + '/log')  # Create the log directory if it did not already exist
+        os.mkdir(str(os.getcwd()) + '\\log')  # Create the log directory if it did not already exist
         log_format()  # define log format
 
 
 def log_format():
     timestr = time.strftime("%Y-%m-%d_%H-%M-%S")
-    logging.basicConfig(filename="log/" + str(timestr) + "-log.txt", level=logging.INFO,
+    logging.basicConfig(filename="log\\" + str(timestr) + "-log.txt", level=logging.INFO,
                         format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 
